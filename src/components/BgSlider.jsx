323a53497e8ef905a2385fce258203233882a5b8
Myselfsayan <sayanghosh8702@gmail.com>
2026-03-31 09:39:06 +0530

    Add BgSlider component and update Home page to include it; enhance Steps component layout and styling

---
+++ src/components/BgSlider.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function BgSlider() {
+    return (
+        <div>
+        
+        </div>
+    )
+}
+
+export default BgSlider
--- src/components/Steps.jsx
+++ src/components/Steps.jsx
@@ -4,20 +4,40 @@ import { assets } from '../assets/assets'
 function Steps() {
     return (
         <div className="mx-4 lg:mx-44 py-20 xl:py-40">
-            <h1 className="text-center text-2xl md:text-3xl lg:text-4xl mt-4 font-semibold">
+            <h1 className="text-center text-2xl md:text-3xl lg:text-4xl mt-4 mb-1 pb-1 font-semibold bg-gradient-to-r from-gray-900 to-gray-400 bg-clip-text text-transparent">
             Steps to remove background <br /> image in seconds
             </h1>
 
-            <div>
-            <div>
-                <img src={assets.upload_icon} alt="" />
-                <div>
-                <p>Upload image</p>
-                <p>
-                    This is a demo text, will replace it later. This is a demo..
-                </p>
+            <div className="flex items-start flex-wrap gap-4 mt-16 xl:mt-24 justify-center">
+                <div className="flex items-start gap-4 bg-white  drop-shadow-md p-7 pb-10 rounded hover:scale-105 transition-all duration-500">
+                    <img className='max-w-9' src={assets.upload_icon} alt="" />
+                    <div>
+                        <p className='text-xl font-medium'>Upload image</p>
+                        <p className='text-sm text-neutral-500 mt-1'>
+                            Select your image <br/> to get started.
+                        </p>
+                    </div>
                 </div>
-            </div>
+
+                <div className="flex items-start gap-4 bg-white  drop-shadow-md p-7 pb-10 rounded hover:scale-105 transition-all duration-500">
+                    <img className='max-w-9' src={assets.remove_bg_icon} alt="" />
+                    <div>
+                        <p className='text-xl font-medium'>Remove Background</p>
+                        <p className='text-sm text-neutral-500 mt-1'>
+                            Our tool instantly removes <br/>the background.
+                        </p>
+                    </div>
+                </div>
+
+                <div className="flex items-start gap-4 bg-white  drop-shadow-md p-7 pb-10 rounded hover:scale-105 transition-all duration-500">
+                    <img className='max-w-9' src={assets.download_icon} alt="" />
+                    <div>
+                        <p className='text-xl font-medium'>Download Image</p>
+                        <p className='text-sm text-neutral-500 mt-1'>
+                            Get your final image <br/> ready to use.
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
 )
